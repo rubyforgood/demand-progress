4d692a4f3393c5fa34b1400119da81bfef960904
Amy Detwiler <adetwiler@barquin.com>
2018-06-09 00:05:41 -0400

    The event listing component works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'events/index'
   resources :committees
+  resources :events
+  root to: "events#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2018_06_08_182158) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
