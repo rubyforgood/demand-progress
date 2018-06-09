bd35ed95607e8b2759c30c797fbf203f68eaf9fc
compostbrain <joel.cahalan@gmail.com>
2018-06-09 14:35:30 -0400

    Add agenda_by_date method for UI formatting

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,19 @@ class Event < ApplicationRecord
   end
 
   scope :by_occurs_at, -> { order(:occurs_at) }
+
+  def upcoming_agenda_dates
+    Event.this_week_forward.pluck(Arel.sql("distinct date(occurs_at)"))
+  end
+
+  def upcoming_events
+    Event.this_week_forward.sort_by(&:occurs_at)
+  end
+
+  def self.agenda_by_date
+    events_array = upcoming_agenda_dates.map{ |date| { agenda_date: date, events: upcoming_events.select{ |event| event.occurs_at.to_date == date }
+      }}
+    return events_array
+  end
+
 end
