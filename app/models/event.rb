999551f33e42ba107926790acb6d3a8e5194e619
compostbrain <joel.cahalan@gmail.com>
2018-06-09 15:08:41 -0400

    Move to private methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,18 +7,20 @@ class Event < ApplicationRecord
 
   scope :by_occurs_at, -> { order(:occurs_at) }
 
-  def upcoming_agenda_dates
-    Event.this_week_forward.pluck(Arel.sql("distinct date(occurs_at)"))
-  end
-
-  def upcoming_events
-    Event.this_week_forward.sort_by(&:occurs_at)
-  end
-
   def self.agenda_by_date
-    events_array = upcoming_agenda_dates.map{ |date| { agenda_date: date, events: upcoming_events.select{ |event| event.occurs_at.to_date == date }
+    upcoming_agenda_dates.map{ |date| { agenda_date: date, events: upcoming_events.select{ |event| event.occurs_at.to_date == date }
       }}
-    return events_array
   end
+
+  private
+
+    def upcoming_agenda_dates
+      Event.this_week_forward.pluck(Arel.sql("distinct date(occurs_at)"))
+    end
+
+    def upcoming_events
+      Event.this_week_forward.sort_by(&:occurs_at)
+    end
+
 
 end
