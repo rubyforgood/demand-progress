3263362e8526e804e9a47746f91b883c001c22bb
compostbrain <joel.cahalan@gmail.com>
2018-06-10 10:47:09 -0400

    nesting committee info into props

--- app/javascript/packs/AgendaList/daily_agenda.js
+++ app/javascript/packs/AgendaList/daily_agenda.js
@@ -1,52 +1,77 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Event from './event'
+import React from "react";
+import PropTypes from "prop-types";
+import Event from "./event";
 
-export function foo(events){
-  return events.map( (event) => {return <Event event={event} key={event.id} />})
+export function foo(events) {
+  return events.map(event => {
+    return <Event event={event} key={event.id} />;
+  });
 }
 
-export default function DailyAgenda({agenda_date, events}){
+export default function DailyAgenda({ agenda_date, events }) {
   let returnedEvents = foo(events);
-  return(
+  return (
     <div className="daily-agenda-section">
       <div>{agenda_date}</div>
       <div>{returnedEvents}</div>
     </div>
-  )
+  );
 }
 
 DailyAgenda.propTypes = {
   agenda_date: PropTypes.string,
-  events: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    topic: PropTypes.string,
-    occurs_at: PropTypes.string,
-    location: PropTypes.string,
-    committee_name: PropTypes.string,
-    subcommittee_name: PropTypes.string
-  }))
-}
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      topic: PropTypes.string,
+      occurs_at: PropTypes.string,
+      location: PropTypes.string,
+      committee_info: PropTypes.arrayOf(
+        PropTypes.shape({
+          committee_name: PropTypes.string,
+          subcommittee_name: PropTypes.string,
+          committee_id: PropTypes.string
+        })
+      )
+    })
+  )
+};
 
 DailyAgenda.defaultProps = {
-  agenda_date: '2018-06-09',
+  agenda_date: "2018-06-09",
   events: [
     {
       id: 5,
-      topic: 'topic',
-      occurs_at: '2018-06-09 09:12:23',
-      location: 'MZ 231',
-      committee_name: 'Committee One',
-      subcommittee_name: 'subcommittee name'
+      topic: "topic",
+      occurs_at: "2018-06-09 09:12:23",
+      location: "MZ 231",
+      committee_info: [
+        {
+          committee_name: "Committee One",
+          subcommittee_name: "subcommittee name",
+          committee_id: "ND-2020"
+        }
+      ]
     },
     {
       id: 6,
-      topic: 'topic',
-      occurs_at: '2018-06-09 09:12:23',
-      location: 'MZ 231',
-      topic: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec dui a leo mattis facilisis. Nulla eget massa vel elit ullamcorper laoreet. Nullam non ipsum imperdiet, efficitur metus vel, malesuada nisl. Curabitur laoreet, sem sit amet pellentesque tempor, est risus varius lacus, eget molestie elit ipsum a mauris.",
-      committee_name: 'Committee Two',
-      subcommittee_name: 'subcommittee name'
+      topic: "topic",
+      occurs_at: "2018-06-09 09:12:23",
+      location: "MZ 231",
+      topic:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec dui a leo mattis facilisis. Nulla eget massa vel elit ullamcorper laoreet. Nullam non ipsum imperdiet, efficitur metus vel, malesuada nisl. Curabitur laoreet, sem sit amet pellentesque tempor, est risus varius lacus, eget molestie elit ipsum a mauris.",
+      committee_info: [
+        {
+          committee_name: "Committee Two",
+          subcommittee_name: "subcommittee name two",
+          committee_id: "ND-2022"
+        },
+        {
+          committee_name: "Committee Two",
+          subcommittee_name: "subcommittee name three",
+          committee_id: "ND-2022"
+        }
+      ]
     }
   ]
-}
+};
--- app/javascript/packs/AgendaList/event.js
+++ app/javascript/packs/AgendaList/event.js
@@ -1,40 +1,60 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Moment from 'react-moment';
-import 'moment-timezone';
+import React from "react";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import "moment-timezone";
 
-export function formatSubcommitteeTitle(subcommittee_name){
-  if (subcommittee_name !== undefined){
-    return (<span className="subcommittee-title col-12 col-md-8">{subcommittee_name}</span>)
-  }
-}
-export function formatOccurDate(occurs_at){
-  return (
-    <Moment format="MM/DD/YYYY hh:mm A">
-      {occurs_at}
-    </Moment>
-    )
-}
+const keygen = (committee_id, subcommittee_name) => {
+  return `${committee_id}-${subcommittee_name}`;
+};
 
-export default function Event({event}){
-  let formattedSubcommitteeTitle = formatSubcommitteeTitle(event.subcommittee_name)
-  let formattedOccursAt = formatOccurDate(event.occurs_at)
-  return (
-    <div key="{event.id}" className="event jumbotron row">
-      <div className="committee-section col-12 col-md-8">
-        <span className="committee-title col-12 col-md-8">{event.committee_name}</span>
+export function formatCommitteeInfo(committee_info) {
+  return committee_info.map(event => {
+    let formattedSubcommitteeTitle = formatSubcommitteeTitle(
+      event.subcommittee_name
+    );
+    return (
+      <div
+        key={keygen(
+          event.committee_info.committee_id,
+          event.committee_info.subcommittee_name
+        )}
+        className="committee-section col-12 col-md-8"
+      >
+        <span className="committee-title col-12 col-md-8">
+          {event.committee_name}
+        </span>
         {formattedSubcommitteeTitle}
       </div>
+    );
+  });
+}
+export function formatSubcommitteeTitle(subcommittee_name) {
+  if (subcommittee_name !== undefined) {
+    return (
+      <span className="subcommittee-title col-12 col-md-8">
+        {subcommittee_name}
+      </span>
+    );
+  }
+}
+export function formatOccurDate(occurs_at) {
+  return <Moment format="MM/DD/YYYY hh:mm A">{occurs_at}</Moment>;
+}
+
+export default function Event({ event }) {
+  let formattedOccursAt = formatOccurDate(event.occurs_at);
+  let formattedCommitteeInfo = formatCommitteeInfo(event.committee_info);
+  return (
+    <div key="{event.id}" className="event jumbotron row">
+      {formattedCommitteeInfo}
       <span className="location-section col-12 col-md-3 ">
         <div className="location-title font-weight-bold">Location</div>
         <div className="location">{event.location}</div>
       </span>
       <span className="topic col-12 col-md-8">{event.topic}</span>
-      <span className="occurs-at col-12 col-md-8">
-        {formattedOccursAt}
-      </span>
+      <span className="occurs-at col-12 col-md-8">{formattedOccursAt}</span>
     </div>
-  )
+  );
 }
 
 Event.propTypes = {
@@ -43,19 +63,30 @@ Event.propTypes = {
     topic: PropTypes.string,
     occurs_at: PropTypes.string,
     location: PropTypes.string,
-    committee_name: PropTypes.string,
-    subcommittee_name: PropTypes.string
+    committee_info: PropTypes.arrayOf(
+      PropTypes.shape({
+        committee_name: PropTypes.string,
+        subcommittee_name: PropTypes.string,
+        committee_id: PropTypes.string
+      })
+    )
   })
-}
+};
 
 Event.defaultProps = {
   event: [
     {
-      committee_name: "This is a committee name",
-      subcommittee_name: "This is a subcommittee name",
-      topic: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec dui a leo mattis facilisis. Nulla eget massa vel elit ullamcorper laoreet. Nullam non ipsum imperdiet, efficitur metus vel, malesuada nisl. Curabitur laoreet, sem sit amet pellentesque tempor, est risus varius lacus, eget molestie elit ipsum a mauris.",
+      committee_info: [
+        {
+          committee_name: "This is a committee name",
+          subcommittee_name: "This is a subcommittee name",
+          committee_id: "MD-2020"
+        }
+      ],
+      topic:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec dui a leo mattis facilisis. Nulla eget massa vel elit ullamcorper laoreet. Nullam non ipsum imperdiet, efficitur metus vel, malesuada nisl. Curabitur laoreet, sem sit amet pellentesque tempor, est risus varius lacus, eget molestie elit ipsum a mauris.",
       location: "MVC-101",
-      occurs_at: '2018-07-01 23:00:00'
+      occurs_at: "2018-07-01 23:00:00"
     }
   ]
-}
+};
